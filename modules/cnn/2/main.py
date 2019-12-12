742899e72ac6ceaf5c5ac31d3367a70e5df70fae
jeremi gendron <gendronjeremi@gmail.com>
2019-12-11 21:36:17 -0500

    wow, it actually trains :O... slow af though, I have so much data in here :P Maybe I need to convolve more, reduce filters, not as many hidden units... lets see!

--- modules/cnn/2/main.py
+++ modules/cnn/2/main.py
@@ -9,35 +9,27 @@ labels = np.load('./data/labels_shifted.npy', allow_pickle=True)
 
 print(labels.shape, data.shape)
 
-exit()
-"""
 model = keras.Sequential([
-  keras.layers.Conv1D(32, 4, strides=1, input_shape=(2000,2), batch_size=400),
-  keras.layers.Activation('relu', a=0.01),
+  keras.layers.Conv1D(32, 4, strides=1, activation='relu', input_shape=(2000,2), batch_size=400),
   keras.layers.BatchNormalization(),
-  keras.layers.Maxpooling1D(2, 2),
-  keras.layers.Conv1D(64, 4, strides=1),
-  keras.layers.Activation('relu', a=0.01),
+  keras.layers.MaxPooling1D(2, 2),
+  keras.layers.Conv1D(64, 4, strides=1, activation='relu'),
   keras.layers.BatchNormalization(),
-  keras.layers.Maxpooling1D(2, 2),
-  keras.layers.Upsampling1D(2),
-  keras.layers.Conv1D(96, 4, strides=1),
-  keras.layers.Activation('relu', a=0.01),
+  keras.layers.MaxPooling1D(2, 2),
+  keras.layers.UpSampling1D(2),
+  keras.layers.Conv1D(96, 4, strides=1, activation='relu'),
   keras.layers.BatchNormalization(),
   keras.layers.Flatten(),
-  keras.layers.Dense(512)
-  keras.layers.Activation('relu', a=0.01),
-  keras.layers.Dense(512)
-  keras.layers.Activation('relu', a=0.01),
+  keras.layers.Dense(512, activation='relu'),
+  keras.layers.Dense(512, activation='relu'),
   keras.layers.Dense(1, activation='sigmoid')
 ])
 
 callbacks = [
-  keras.callbacks.callbacks.ModelCheckpoint('models/', monitor='val_acc', verbose=0, save_best_only=True, mode='auto', period=3)
-  keras.callbacks.callbacks.EarlyStopping(monitor='val_acc', min_delta=0.001, patience=0, verbose=0, mode='auto', restore_best_weights=False)
+  keras.callbacks.ModelCheckpoint('models/', monitor='val_acc', verbose=0, save_best_only=True, mode='auto', period=3),
+  keras.callbacks.EarlyStopping(monitor='val_acc', min_delta=0.001, patience=0, verbose=0, mode='auto', restore_best_weights=False)
 ]
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train, Y_train, validation_split=0.1)
-"""
+model.fit(data, labels, validation_split=0.1)
